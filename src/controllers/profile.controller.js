b06e5e547f315b7fdbb88894b975b1ec7f5478d5
Swapnil Opash <sra.opash@gmail.com>
2024-05-16 18:23:28 +0530

    fixed

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -92,7 +92,7 @@ exports.updateProfile = async function (req, res) {
         };
         await Profile.images(data);
       }
-      if (req.body?.interests.length) {
+      if (req.body?.interests) {
         await User.addInterest(req.body.interests, profileId, []);
       }
       profile.updatedDate = moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
